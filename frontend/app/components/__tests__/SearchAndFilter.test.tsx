a12816610b54c8b29fe8ed7ada42f04f2ebc9cde
holydiver71 <andy.shutt@gmail.com>
2026-01-21 20:38:50 +0000

    test: restore fireEvent imports and mock useRouter in SidebarHeaderOffset tests

--- frontend/app/components/__tests__/SearchAndFilter.test.tsx
+++ frontend/app/components/__tests__/SearchAndFilter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { SearchAndFilter } from '../SearchAndFilter';
 import * as api from '../../lib/api';
 
