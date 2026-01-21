6ea58a5b756f68ffaf19e996761821fd4c330af8
holydiver71 <andy.shutt@gmail.com>
2026-01-21 20:34:14 +0000

    chore(test): remove unused fireEvent imports

--- frontend/app/components/__tests__/EditReleaseButton.test.tsx
+++ frontend/app/components/__tests__/EditReleaseButton.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { EditReleaseButton } from '../EditReleaseButton';
 import { useRouter } from 'next/navigation';
 
